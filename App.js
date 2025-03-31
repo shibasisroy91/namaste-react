27c9d5e2c09bc1d901eec3349b232bb06e811520
Shibasis Roy <srstyles.dev@gmail.com>
2025-03-31 20:30:45 +0530

    Ignite our app by adding npm

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "H1 tag"),
-    React.createElement("h2", { id: "heading2" }, "H2 tag"),
+  React.createElement("div", { id: "child", key: "child" }, [
+    React.createElement("h1", { id: "heading", key: "h1" }, "H1 tag"),
+    React.createElement("h2", { id: "heading2", key: "h2" }, "H2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h3", { id: "heading3" }, "H3 tag"),
-    React.createElement("h4", { id: "heading4" }, "H4 tag"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h3", { id: "heading3", key: "h3" }, "H3 tag"),
+    React.createElement("h4", { id: "heading4", key: "h4" }, "H4 tag"),
   ]),
 ]);
 
